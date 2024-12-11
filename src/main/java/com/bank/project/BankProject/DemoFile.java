8f0f8f48b8c737625df111558ce4a00a23ffbd7d
shusain1992 <shusain.java@gmail.com>
2024-12-11 15:07:20 +0530

    changes

--- src/main/java/com/bank/project/BankProject/DemoFile.java
+++ src/main/java/com/bank/project/BankProject/DemoFile.java
@@ -4,11 +4,7 @@ public class DemoFile {
 
 		public static void main(String args[]) {
 			int name=45;
-			int name1=45;
-			int name2=45;
-			int name3=45;
-			int name4=45;
-			int name5=45;
+			
 			System.out.println("customer Name "+name);	
 		}
 }
